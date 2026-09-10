5bb62c465f5bab37c540f9d302a66875848c919e
Eissa Soubhi <eissa.soubhi@gmail.com>
2026-09-10 20:30:29 +0200

    fix: remove incompatible ESM preload

--- apps/desktop/src/main/preload.ts
+++
@@ -1,9 +0,0 @@
-import { contextBridge, ipcRenderer } from 'electron';
-
-contextBridge.exposeInMainWorld('companion', {
-  platform: process.platform,
-  microphone: {
-    getPermissionStatus: () => ipcRenderer.invoke('microphone:get-permission'),
-    requestPermission: () => ipcRenderer.invoke('microphone:request-permission'),
-  },
-});
